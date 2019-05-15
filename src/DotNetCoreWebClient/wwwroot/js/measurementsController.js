20010ecc5d98b9f1e4a093dc4cfca8d71c6349c6
JustD4nTe <domikxf@gmail.com>
2019-05-15 13:45:34 +0200

    Save added record into db

--- src/DotNetCoreWebClient/wwwroot/js/measurementsController.js
+++ src/DotNetCoreWebClient/wwwroot/js/measurementsController.js
@@ -11,7 +11,10 @@
 
         this._newMeasurementSection.addEventListener(new class {
             newMeasurementAdded(e) {
-                _this._measurementListSection.addNewMeasurement(e)
+                let data = e
+                data.createdBy = "Operator"
+                data.createdAt = "2019-05-08T00:00:00"
+                _this._service.post(data)
             }
         })
 
@@ -19,12 +22,20 @@
             getResponseReady(e) {
                 JSON.parse(e.data).forEach(i => {
                     _this._measurementListSection.addNewMeasurement({
+                        id:  i.id,
                         name: i.name,
-                        value: i.value
+                        value: i.value,
+                        createdBy: i.createdBy
                     })
                 })
             }
+
+            postResponseReady(e) {
+                _this._measurementListSection.addNewMeasurement(JSON.parse(e.data))
+            }
         })
+
+
 
         this._service.get()
     }
